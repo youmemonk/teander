fc7aaed402c1d03eb1224c64ec62934da197bd46
youmemonk <ukeym160@gmail.com>
2022-01-08 14:26:03 +0530

    [init] v1 files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Building the Tinder Clone🚀</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Tinder Card */}
+      {/* Buttons below Tinder Card */}
+
+      {/* Chat Screen */}
+      {/* Individual Chat Screen */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            I am a header.
+        </div>
+    )
+}
+
+export default Header
